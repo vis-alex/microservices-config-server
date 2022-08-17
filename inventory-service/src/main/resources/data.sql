1390ae49cde389c9b22715c82fb49ccab34218b3
vis-alex <usenkoshurik@mail.ru>
2022-08-17 10:50:36 +0300

    fix

--- inventory-service/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-# INSERT INTO inventory (id, scu_code, stock)
-# VALUES (1, 'IPHONE_12_RED', 100);
-#
-# INSERT INTO inventory (id, scu_code, stock)
-# VALUES (2, 'IPHONE_12_GREY', 0);
--- inventory-service/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-# CREATE TABLE inventory
-# (
-#     id       INT    NOT NULL AUTO_INCREMENT,
-#     scu_code varchar(45) NOT NULL,
-#     stock    INTEGER NOT NULL,
-#     PRIMARY KEY (id)
-# );
